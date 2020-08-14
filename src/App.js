682bb20d135fa8b0ec021092b123e325b6ead0e8
Jo Seong Won <tjddnjs9497@naver.com>
2020-08-14 17:06:12 +0900

    PreViewContainer 적용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Switch, Route } from "react-router-dom";
 import HeaderContainer from './containers/HeaderContainer'
 import Homepage from "./pages/Hompage";
 import FooterContainer from "./containers/FooterContainer";
+import PreViewContainer from './containers/PreViewContainer';
 
 function App() {
   return (
     <div className="App">
       <HeaderContainer />
+      <PreViewContainer />
       <Switch>
         <Route exact path="/" component={Homepage} />
       </Switch>
