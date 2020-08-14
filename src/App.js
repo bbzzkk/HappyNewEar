c0d039e17e4f278d9b7d1d309e600c16f37bc61d
Joylish <yeonzzu2@gmail.com>
2020-08-14 10:36:29 +0900

    초기 reactapp과 mobx 세팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Switch, Route } from "react-router-dom";
+
+import HeaderContainer from './containers/HeaderContainer'
+import Homepage from "./pages/Hompage";
+import FooterContainer from "./containers/FootContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderContainer />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+      <FooterContainer/>
     </div>
   );
 }
