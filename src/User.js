d9b657a77868cc9bfad67314fbfdc1b3acd561e6
JSWww <tjddnjs9497@naver.com>
2020-08-17 18:21:22 +0900

    user5 추가

--- src/User.js
+++ src/User.js
@@ -143,6 +143,62 @@ const User = [
         items: [
         ]
     },
+    {
+        id: 'user5',
+        pw: 'user5',
+        items: [
+            {
+                id: 4,
+                category: 'keyring',
+                name: "무궁화태극기",
+                date: 1597380430048,
+                price: 10000,
+                imageUrl: "images/keyring/4.jpg",
+                checked: true,
+                count: 1
+            },
+            {
+                id: 16,
+                category: 'case',
+                name: "귀여운토끼",
+                date: 1597371630048,
+                price: 20000,
+                imageUrl: "images/case/16.jpg",
+                checked: true,
+                count: 1
+            },
+            {
+                id: 10,
+                category: 'sticker',
+                name: "땡땡이핑크",
+                date: 1597390230031,
+                price: 2000,
+                imageUrl: "images/sticker/2.jpg",
+                checked: true,
+                count: 1
+            },
+            {
+                id: 1,
+                category: 'case',
+                name: "아보카도냥이",
+                date: 1597370030048,
+                price: 10000,
+                imageUrl: "images/case/1.jpg",
+                checked: true,
+                count: 1
+            },
+            {
+                id: 5,
+                category: 'keyring',
+                name: "겨울왕국",
+                date: 1597380530048,
+                price: 10000,
+                imageUrl: "images/keyring/5.jpg",
+                checked: true,
+                count: 1
+            },
+        ]
+    },
 ]
 
 export default User;
