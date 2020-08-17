41cc4ba9866d66cb4aa1a24ef2c5179bf6cc4fa1
hataerin <goil1113@likelion.org>
2020-08-17 15:47:34 +0900

    상품상세페이지 작성 1. Product.js파일 위치 변경> /src/Product.js 2. Product의 개별상품 정보 화면에 출력

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Switch, Route } from "react-router-dom";
 import HeaderContainer from './containers/HeaderContainer'
 import Homepage from "./pages/Hompage";
 import CartPage from "./pages/CartPage";
+import DetailPage from "./pages/DetailPage";
 import FooterContainer from "./containers/FooterContainer";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/cart" component={CartPage} />
+        <Route path="/detail" component={DetailPage} />
       </Switch>
       <FooterContainer/>
     </div>
---
+++ src/pages/DetailPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import DetailContainer from '../containers/DetailContainer';
+
+class DetailPage extends Component{
+    render(){
+        return(
+            <DetailContainer />
+        )
+    }
+}
+
+export default DetailPage
