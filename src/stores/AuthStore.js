4892ad9b2429fa6bb9d930126caa59d130d4c5de
Joylish <yeonzzu2@gmail.com>
2020-08-17 20:21:57 +0900

    로그인 및 회원가입 관련 mobx stores 구현

---
+++ src/stores/AuthStore.js
@@ -0,0 +1,79 @@
+import { action, observable } from "mobx";
+
+import { auth, signInWithGoogle, createUserProfileDocument } from "../firebase/firebase.utils";
+
+export default class AuthStore {
+  constructor(root) {
+    this.root = root;
+  }
+  
+  @observable signInEmail= ''
+  @observable signInPassword= ''
+
+  @observable displayName = ''
+  @observable signUpEmail= ''
+  @observable signUpPassword= ''
+  @observable confirmedPassword= ''
+
+  
+  @observable currentUser = null;
+  @observable unsubscribeFromAuth = null;
+
+  @action
+  setCurrentUser = (user) => {
+      this.currentUser = user;
+  };
+
+  @action
+  setUnsubscribeFromAuth = (unsubscribe) => {
+      this.unsubscribeFromAuth = unsubscribe;
+  };
+
+  @action
+  changeSignInForm = (name, value) =>{
+    this[name] = value
+  }
+
+  @action signIn = async (email, password) =>{
+    try{
+      await auth.signInWithEmailAndPassword(email, password)
+    }catch(error){
+      console.error(error);
+    } finally{
+       this.root.authStore.signInEmail = "";
+       this.root.authStore.signInPassword = "";
+    } 
+  }
+
+  @action signInWithGoogle = () =>{
+    signInWithGoogle()
+  }
+
+  @action signOut = () =>{
+    auth.signOut()
+  }
+
+  @action signUp = async () =>{
+    const {
+      signUpEmail,
+      signUpPassword,
+      confirmedPassword,
+      displayName,
+    }= this
+    if(signUpPassword !== confirmedPassword){
+      alert('입력하신 비밀번호가 서로 다릅니다.')
+    }
+    try{
+      const {user} = await auth.createUserWithEmailAndPassword(signUpEmail, signUpPassword);
+      
+      await createUserProfileDocument(user, {displayName})
+      
+      this.signUpEmail = "";
+      this.signUpPassword = "";
+      this.confirmedPassword = "";
+      this.displayName = "";
+    }catch(error){
+      console.error(error.message)
+    }
+  }
+}
