e12a32c202e11d7ba8516458ac8d53f4373e973a
Joylish <yeonzzu2@gmail.com>
2020-08-17 15:20:54 +0900

    로그인 뷰 컴포넌트 구성

---
+++ src/components/LoginForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+// import InputFormContainer from '../containers/InputFormContainer'
+
+class LoginForm extends Component{
+    render(){
+        const {email, password, handleChange, handleSubmit} = this.props
+        return (
+          <>
+            <h2 className="title">로그인</h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                name="email"
+                type="email"
+                onChange={handleChange}
+                value={email}
+                required
+              />
+              <input
+                name="password"
+                type="password"
+                onChange={handleChange}
+                value={password}
+                required
+              />
+              <div className="login-buttons">
+                <button type="submit">로그인</button>
+              </div>
+            </form>
+          </>
+        );
+    }
+}
+
+export default LoginForm
