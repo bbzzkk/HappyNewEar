4b06f55ed0c0e7a28543a28ec44d4132844c0233
kwak-bs <iam-bs@naver.com>
2020-08-18 15:40:35 +0900

    index.js에서 시맨틱 유아이 삭제

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css';
+
 
 import Root from './Root'
 
