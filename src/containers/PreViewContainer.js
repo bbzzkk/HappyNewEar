7411e773d3f585648f3a2431ed1f9e3c3ee63dee
hataerin <goil1113@likelion.org>
2020-08-18 16:54:12 +0900

    slide 글씨 제거 1. slide 글씨 제거 2. 관심상품페이지 정상화

--- src/containers/PreViewContainer.js
+++ src/containers/PreViewContainer.js
@@ -55,17 +55,17 @@ class PreViewContainer extends Component {
                     <Slide>
                         <div className="each-slide" style={{'backgroundImage': `url(${slideImages[0]})`, height:'700px'}}>
                             <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                            <span>Slide 1</span>
+                           
                             </div>
                         </div>
                         <div className="each-slide" style={{'backgroundImage': `url(${slideImages[1]})`, height:'700px'}}>
                             <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                            <span>Slide 2</span>
+                        
                             </div>
                         </div>
                         <div className="each-slide" style={{'backgroundImage': `url(${slideImages[2]})`, height:'700px'}}>
                             <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                            <span>Slide 3</span>
+                         
                             </div>
                         </div>
                     </Slide>
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -13,26 +13,8 @@ const slideImages = [
 class CartPage extends Component {
     render() {
         return (
-            // <CartContainer/>
-            <div className="slide-container">
-                    <Slide>
-                        <div className="each-slide" style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                            <span>Slide 1</span>
-                            </div>
-                        </div>
-                        <div className="each-slide">
-                            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                            <span>Slide 2</span>
-                            </div>
-                        </div>
-                        <div className="each-slide">
-                            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                            <span>Slide 3</span>
-                            </div>
-                        </div>
-                    </Slide>
-                </div>
+             <CartContainer/>
+           
         );
     }
 }
