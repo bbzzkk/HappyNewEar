821933b37e1a907a4ef86f93811abea43baef7fe
kwak-bs <iam-bs@naver.com>
2020-08-18 09:36:44 +0900

    stage 겨우 완료..

--- src/components/PreViewList.js
+++ src/components/PreViewList.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import Item from './Item';
+import PreViewListItem from './PreViewListItem';
 
 class PreViewList extends Component {
     render() {
-        const { items, itemClick } = this.props;
-        const result = items.map(tmp => {
-            return (
-                <div onClick={itemClick}>
-                    <Item name={tmp.imageUrl}></Item>
-                </div>
-            );
-        });
+        const { items, itemClick, ItemJson, onCategoryClick} = this.props;
+        
+        const prevItems = items.filter(tmp => tmp.id < 5); //초기 4개만 출력하게끔 해줌 하 이거 몇시간 걸렸는지 모르겠네 썅 
 
+        // const result = items.map(tmp => {
+        //     return (
+        //         <div onClick={itemClick}>
+        //             <Item name={tmp.imageUrl}
+        //                     price ={tmp.price}
+        //                     id = {tmp.id}
+        //                     items = {tmp}></Item>
+        //         </div>
+        //     );
+        // });
+
+        const previewItems = prevItems.map((preitems) =>{
+            return (
+               // <Item list={list} onCategoryClick={this.props.onCategoryClick}/>
+               <div>
+                   <PreViewListItem img = {preitems.imageUrl}
+                         price = {preitems.price}
+                         id = {preitems.id}
+                         >
+                         </PreViewListItem>
+               </div>
+            );
+        })
         return (
-            result
+           // result
+
+            previewItems
+
         );
     }
 }
--- src/containers/PreViewContainer.js
+++ src/containers/PreViewContainer.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
+import { observer, inject } from "mobx-react";
 import PreViewList from '../components/PreViewList'
-import ItemJson from '../ItemJson';
+import ListContainer from './ListContainer';
+import { Link } from "react-router-dom";
+
+@inject("HomeStore")
+@observer
 
 class PreViewContainer extends Component {
-    categoryClick = () => {
+    
+    OnCategoryClick = (id) => {
         console.log("categoryClick");
+        this.props.HomeStore.CategoryClick(id);
     }
 
     itemClick = () => {
         console.log("itemClick");
     }
-
     render() {
-        const item = ItemJson;
-
-        const previewList = item.map(list => {
+        const { ItemJson } = this.props.HomeStore;
+        const previewList = ItemJson.map(list => {
             return (
                 <div>
-                    <h1 onClick={this.categoryClick}>{list.category}</h1>
-                    <PreViewList items={list.items} itemClick={this.itemClick}></PreViewList>
+                   <Link to="/list">
+                   <h1 onClick={() => this.OnCategoryClick(list.id)}>{list.category}</h1>
+                   </Link> 
+                    
+                    <PreViewList
+                        items={list.items}
+                        >
+                    </PreViewList>
                 </div>
-            );
-        });
-
+            )
+        })
         return (
             previewList
         );
