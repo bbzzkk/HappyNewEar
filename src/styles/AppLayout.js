a9c8687d3a474c4a81e149e276f321e6359f8670
JSWww <tjddnjs9497@naver.com>
2020-08-19 02:57:06 +0900

    푸터, 헤더 css 정리

--- src/styles/AppLayout.js
+++ src/styles/AppLayout.js
@@ -1,8 +1,14 @@
 import styled from "@emotion/styled";
+import { GridColumn } from "semantic-ui-react";
 
 const AppLayout = styled.div((props) => ({
     fontcolor: 'black',
-    scrollbarWidth: 'none' /* Firefox */
+    scrollbarWidth: 'none', /* Firefox */
+    // height: '100%',
+    // width: '100%',
+    margin: '0',
+    display: 'flex',
+    flexDirection: 'column',
 }))
 
 export default AppLayout;
--- src/styles/FooterLayout.js
+++ src/styles/FooterLayout.js
@@ -1,9 +1,22 @@
 import styled from '@emotion/styled';
 
 const FooterWrapper = styled.div(props => ({
-    display: 'block',
     backgroundColor: '#fff',
     textAlign: 'center',
+    marginTop: '3em',
+    paddingTop: '2.0em',
+    paddingBottom: '2em',
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '15px',
+    borderTop: '1px solid black',
+    // background: 'lightgray',
+
+    ".address": {
+    },
+    ".copyright": {
+        margin: "5px",
+      },
 }))
 
 export {
