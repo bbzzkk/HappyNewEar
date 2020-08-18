981769cd7930963f7101590bc9363b97638261be
kwak-bs <iam-bs@naver.com>
2020-08-18 09:37:15 +0900

    주석삭제함 ^--^

--- src/components/PreViewList.js
+++ src/components/PreViewList.js
@@ -5,22 +5,10 @@ class PreViewList extends Component {
     render() {
         const { items, itemClick, ItemJson, onCategoryClick} = this.props;
         
-        const prevItems = items.filter(tmp => tmp.id < 5); //초기 4개만 출력하게끔 해줌 하 이거 몇시간 걸렸는지 모르겠네 썅 
-
-        // const result = items.map(tmp => {
-        //     return (
-        //         <div onClick={itemClick}>
-        //             <Item name={tmp.imageUrl}
-        //                     price ={tmp.price}
-        //                     id = {tmp.id}
-        //                     items = {tmp}></Item>
-        //         </div>
-        //     );
-        // });
+        const prevItems = items.filter(tmp => tmp.id < 5);
 
         const previewItems = prevItems.map((preitems) =>{
             return (
-               // <Item list={list} onCategoryClick={this.props.onCategoryClick}/>
                <div>
                    <PreViewListItem img = {preitems.imageUrl}
                          price = {preitems.price}
@@ -31,10 +19,7 @@ class PreViewList extends Component {
             );
         })
         return (
-           // result
-
             previewItems
-
         );
     }
 }
